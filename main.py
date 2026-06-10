bc461d6a0d02c303442ca435aeb739d4d322ece4
Kevin J Medina <kevin@Kevins-MacBook-Air-5.local>
2026-06-09 22:55:47 -0400

    Implemented WeatherClient and GeocodingClient classes to seperate concerns.

--- main.py
+++ main.py
@@ -1,105 +1,21 @@
 import typer
-import httpx
 from rich.console import Console
 from rich.panel import Panel
-from dataclasses import dataclass
-
+from src.api.geocoding_api import GeocodingClient
+from src.api.weather_api import WeatherClient  
 
 console = Console()
-
-#console.print(Panel("Hello, [red]World!"))
-
-geocoding_params = {
-    "name" : str
-}
-
 app = typer.Typer()
 
-
-#======================================
-#          Calling the API 
-#======================================
-def geocoding_info(city : str, verbose:bool = False):
-    geocoding_params["name"] = city.lower()
-    try:
-        geocoding_resp = httpx.get("https://geocoding-api.open-meteo.com/v1/search", params=geocoding_params)
-        geocoding_data = geocoding_resp.json()
-
-        if verbose:
-            console.rule("GEOCODING API RESPONSE")
-            console.print_json(data=geocoding_data["results"][0])
-            console.rule()
-
-        geocoding_latitude = geocoding_data["results"][0]["latitude"]
-        geocoding_longitude = geocoding_data["results"][0]["longitude"]
-        return (geocoding_latitude, geocoding_longitude)
-    except Exception as error:
-        console.print(f"[red]Geocoding Error: {error}[/red]")
-        console.print(f"City - {city}, not found. Try again.")
-        exit(1)
-
-
-
-def weather_caller(latitude, longitude, verbose:bool = False, url = "https://api.open-meteo.com/v1/forecast", params = {
-    "latitude" : 25.823131,
-    "longitude" : -80.2256272
-}):
-    try:
-        weather_resp = httpx.get(url, params=params)
-        weather_data = weather_resp.json() 
-        if verbose:
-            console.rule("API RESPONSE")
-            console.print_json(data=weather_data)
-            console.rule()
-        return weather_data
-    except Exception as e:
-       console.print(f"Weather API ERROR: {e}")
-
-#==============================================
-#        Creating Models and Loading Data
-#===============================================
-
-@dataclass
-class CurrentTemperature:
-    current_temperature : float
-    feels_like_temperature : float
-    time : str
-    units : str
-
-def current_temperature_loader(weather_data, verbose:bool):
-    return CurrentTemperature(
-        current_temperature = weather_data["current"]["temperature_2m"],
-        feels_like_temperature = weather_data["current"]["apparent_temperature"],
-        time = weather_data["current"]["time"],
-        units = weather_data["current_units"]["temperature_2m"]
-    )
-   
-
-#current_temp = current_temperature_loader(weather_caller("Miami"))
-#console.print(current_temp)
-
-#================================================
-#Display Current Temperature
-#================================================
-def display_current_temperature(current_temp: CurrentTemperature):
-    console.print(Panel(f"[{current_temp.time}] - Current temperature: {current_temp.current_temperature}{current_temp.units}, 'Feels Like': {current_temp.feels_like_temperature}{current_temp.units}", ))
-
 @app.command()
-def current_temperature(city: str = "Miami", verbose:bool = False):
+def display_current_temperature(city:str = "Miami"):
+    geocoding_client = GeocodingClient(base_url = "https://geocoding-api.open-meteo.com/v1/search")
+    coordinates = geocoding_client.get_coordinates(city)
 
-    weather_params = {
-        "latitude": None,
-        "longitude":None, 
-        "current": ["temperature_2m", "apparent_temperature"],
-        "timezone": "auto"
-    }
+    weather_client = WeatherClient(base_url = "https://api.open-meteo.com/v1/forecast")
+    current_weather = weather_client.get_current_weather(coordinates)
+    console.print(Panel(f"[{current_weather["time"]}] - Current temperature: {current_weather["temperature"]}C, 'Feels Like': {current_weather["feels_like"]}C", ))
 
-    geocoding_latitude, geocoding_longitude = geocoding_info(city, verbose)
-    weather_params["latitude"] = geocoding_latitude
-    weather_params["longitude"] = geocoding_longitude
-    weather_data = weather_caller(geocoding_latitude, geocoding_longitude, verbose, params=weather_params)
-    current_temperature = current_temperature_loader(weather_data, verbose)
-    display_current_temperature(current_temperature)
 
 
 
--- src/api/weather_api.py
+++ src/api/weather_api.py
@@ -1,61 +1,55 @@
-from dataclasses import dataclass
+from pydantic import BaseModel
+import httpx
+from src.api.geocoding_api import Coordinates
 
 
-@dataclass
-class CurrentWeather:
-    temperature : float # Actual temperature in C or F
-    feels_like_temperature : float # Apperent temperature in C or F
-    wind_speed : float # Wind speed in MPH or KPH
-    cloud_cover : int # Percentage of cloud cover at the current latitude and longitude
-    uv_index : float  
-    time: str  # Format: YYYY-MM-DD
+class WeatherClient:
+    """
+        Concerns: Communicating with the API and Transforming Raw API response into dataclasses/models
+    """ 
 
-    
-@dataclass
-class HourForecast:
-    hour : str
+    def __init__(self, http_client : httpx.Client | None = None, base_url : (str|None) = None, timeout: float = 10.0):
+        self.base_url = base_url
+        self.timout = timeout
+        if http_client is None:
+            self._client = httpx.Client(base_url = base_url, timeout = timeout)
+        else:
+            self._client = http_client
+
+    def _make_request(self, url_parameters:dict):
+        try:
+            response = self._client.get(self.base_url, params=url_parameters)
+            return response
+        except httpx.RequestError as err:
+            print(f"An error occurred while requesting: {err}")
+        
+    def _transform_response(self, response):
+        return response.json()
+   
+    def get_current_weather(self, coordinates: Coordinates) -> CurrentTemperature:
+        params = {
+                    "latitude": coordinates["latitude"],
+                    "longitude": coordinates["longitude"],
+                    "current": ["temperature_2m", "apparent_temperature"],
+                    "timezone": "auto",
+            }
+        response = self._make_request(params)
+        data = self._transform_response(response)
+        current_weather: CurrentTemperature = {
+            "temperature" : data["current"]["temperature_2m"],
+            "feels_like" : data["current"]["apparent_temperature"],
+            "time" : data["current"]["time"]
+        } 
+
+        return current_weather
+
+
+
+
+class CurrentTemperature(BaseModel):
     temperature : float
-    feels_like_temperature : float
-    chance_of_percipitation : float
-    amount_of_percipitation : float
-    cloud_cover : int
-    wind_speed : float
-    uv_index : float
+    feels_like : float
+    time : str 
 
 
-
-
-
-@dataclass
-class DayForecast:
-    day : str
-    hours : list[str] 
-    temperature : list[float]
-    feels_like_temperature : list[float]
-    chance_of_percipitation : list[float]
-    amount_of_percipitation : list[float] #Total precipitation in the preceding hour in mm or inches
-    cloud_cover : list[int]
-    wind_speed : list[float]
-    uv_index : list[float]
-    max_uv_index: float
-    max_temperature: float
-    min_temperature: float
-
-    weather_code : int | None # Specific to the MeteoAPI this 
-
-
-@dataclass
-class MultiDayForecast:
-    multi_day_forecast : list[DayForecast]
-
-@dataclass
-class WeatherReport:
-    current_weather : CurrentWeather
-    multi_day_forecast : MultiDayForecast
-
-
-
-
-
-    
 
